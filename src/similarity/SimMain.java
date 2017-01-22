043814fae6af34d4b711ef2608176c3c43011d70
okok167 <liupuming@live.cn>
2017-01-22 19:18:32 +0800

    first_commit

---
+++ src/similarity/SimMain.java
@@ -0,0 +1,25 @@
+package similarity;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class SimMain {
+		public static void main(String[] args) {
+			 /*ImagePHash p = new ImagePHash();
+			 String image1;
+		     String image2;
+		     try {
+		    	   image1 = p.getHash(new FileInputStream(new File("n.png")));
+		           image2 = p.getHash(new FileInputStream(new File("n.png")));
+		           System.out.println("Score is " + p.distance(image1, image2));
+		           
+		       } catch (FileNotFoundException e) {
+		           e.printStackTrace();
+		       } catch (Exception e) {
+		           e.printStackTrace();
+		       }
+		       */
+		}
+
+	}
